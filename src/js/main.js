738fec49f5aaeb6cbaebf24355766c7eaf01db22
Vlad Basoon <vbasoon@gmail.com>
2021-08-03 11:19:53 +0300

    sidebar.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
-$(function () {
-
-})
+const app = document.getElementById("app");
+console.log(app);
+const sideBtn = document.getElementById("btn-menu");
+console.log(sideBtn);
+const sideBar = document.getElementById("sidebar");
+console.log(sideBar);
+const closeBtn = document.getElementById("btn-close");
+console.log(closeBtn);
